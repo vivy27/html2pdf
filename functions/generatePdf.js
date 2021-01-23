51ecf5d5da5bb070270904684d660743a8de4dd0
vivy27 <vivy27@gmail.com>
2021-01-23 17:49:59 +0100

    remove handlebars package

--- functions/generatePdf.js
+++ functions/generatePdf.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-const hb = require('handlebars')
 
 async function generatePdf(file, options) {
   // we are using headless mode
@@ -11,20 +10,9 @@ async function generatePdf(file, options) {
   });
   const page = await browser.newPage();
 
-  if(file.content) {
-    console.log("Compiling the template with handlebars")
-    // we have compile our code with handlebars
-    const template = hb.compile(file.content, { strict: true });
-    const result = template(file.content);
-    const html = result;
-
-    // We set the page content as the generated html by handlebars
-    await page.setContent(html);
-  } else {
-    await page.goto(file.url, {
-      waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'],
-    });
-  }
+  await page.goto(file.url, {
+    waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'],
+  });
 
   const bufferData = await page.pdf(options);
   await browser.close();
